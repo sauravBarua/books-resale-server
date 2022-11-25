a106320989ae5bfdee3a01be23be2e09be03931e
sauravBarua <myallmailofficial@gmail.com>
2022-11-25 16:30:15 +0100

    added mongodb

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-const express = require('express')
+const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.e7zcqyn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const booksCollectionCategories = client
+      .db("books")
+      .collection("categories");
+
+    app.post("/categories", async (req, res) => {
+      const category = req.body;
+      const result = await booksCollectionCategories.insertOne(category);
+      res.send(result);
+      console.log(result);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
