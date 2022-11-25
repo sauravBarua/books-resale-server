ebef0ede797009270d74dd0c3d99ab5c55f1a247
sauravBarua <myallmailofficial@gmail.com>
2022-11-25 12:59:24 +0100

    added cors dotenv mongodb

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
