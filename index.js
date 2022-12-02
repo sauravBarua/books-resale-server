3999bb5eb3f6d426817efcd3ca4e68aabc806e19
sauravBarua <myallmailofficial@gmail.com>
2022-12-02 10:09:14 +0100

    done

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ async function run() {
       .collection("categories");
     const usersCollection = client.db("books").collection("users");
     const adsCollection = client.db("books").collection("ads");
+    const bookingsCollection = client.db("books").collection("bookings");
 
     app.post("/categories", async (req, res) => {
       const category = req.body;
@@ -96,6 +97,23 @@ async function run() {
       const cursor = booksCollectionCategories.find(query);
       const ads = await cursor.toArray();
       res.send(ads);
+    });
+
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    });
+    app.get("/bookings", async (req, res) => {
+      let query = {};
+      if (req.query.booking) {
+        query = {
+          booking: req.query.bookings,
+        };
+      }
+      const cursor = bookingsCollection.find(query);
+      const bookings = await cursor.toArray();
+      res.send(bookings);
     });
   } finally {
   }
